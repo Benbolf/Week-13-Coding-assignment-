7f3fd4fe8fcc68d16e91bd6c31a070fe7337bde8
Your Name <you@example.com>
2023-05-06 21:01:53 -0500

    first commit

--- week13/src/App.js
+++ week13/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ChannelForm from "./postForm";
+import ChannelList from "./movielist";
 
-function App() {
+let nextPostId = 10;
+let nextChannelId = 10;
+
+export default function App() {
+  const [channelList, setChannelList] = useState([]);
+  const [form, setform] = useState([]);
+
+  const addPost = (newPostData) => {
+    const newPost = {
+      id: nextPostId++,
+      ...newPostData
+    };
+    setform(form.concat(newPost));
+  };
+
+  const addChannel = (newChannelData) => {
+    const newChannel = {
+      id: nextChannelId++,
+      ...newChannelData
+    };
+    setChannelList(channelList.concat(newChannel));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ChannelForm addChannel={addChannel} />
+      <hr />
+      <ChannelList
+        channelList={channelList}
+        form={form}
+        addPost={addPost}
+      />
     </div>
   );
 }
-
-export default App;
